5e55755c0d20e804fab1a138045447c2aa2020f2
andaguiar <130407792+andaguiar@users.noreply.github.com>
2023-05-11 12:29:20 -0300

    Update perguntas.dart
    original

--- perguntas.dart
+++ perguntas.dart
@@ -1,11 +1,93 @@
+import 'package:aula/criar.dart';
 import 'package:flutter/material.dart';
 
-class Perguntas extends StatelessWidget{
-  String text;
-  Perguntas(this.text);
-  @override 
-  Widget build (BuildContext context){
-  return Text(text, style: TextStyle (fontSize: 26),
-  textAlign: TextAlign.center,);
+void main() {
+  runApp(ComponenteInicial());
 }
+
+class ComponenteInicial extends StatefulWidget {
+  @override
+  State<ComponenteInicial> createState() => _ComponenteInicialState();
+}
+
+class _ComponenteInicialState extends State<ComponenteInicial> {
+  var contador = 0;
+  var cont = 0;
+  List<Widget> botoes = [];
+
+  final perguntas = [
+    "signo?",
+    "cor?",
+    "idade?",
+    "sexualidade?",
+  ];
+
+  void eventobotao() {
+    setState(() {
+      contador++;
+    });
+    print(contador);
+  }
+
+  void criarbt() {
+    if (cont < 5) {
+      setState(() {
+        cont++;
+        botoes.add(
+          ElevatedButton(
+            onPressed: eventobotao,
+            child: Text("Botão $cont"),
+          ),
+        );
+      });
+    }
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Perguntas e respostas!"),
+        ),
+        body: Column(
+          children: [
+            Column(
+              children: [
+                perguntas(perguntas[contador]),
+                ElevatedButton(
+                  onPressed: eventobotao,
+                  child: Text("amarelo"),
+                ),
+                ElevatedButton(
+                  onPressed: eventobotao,
+                  child: Text("roxo"),
+                ),
+                ElevatedButton(
+                  onPressed: eventobotao,
+                  child: Text("laranja"),
+                ),
+                ElevatedButton(
+                  onPressed: eventobotao,
+                  child: Text("azul"),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Digite sua resposta',
+                    errorText:
+                        contador == 0 ? 'Este campo é obrigatório' : null,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: criarbt,
+                  child: Text("Criar botão"),
+                ),
+              ],
+            ),
+            Column(children: botoes),
+          ],
+        ),
+      ),
+    );
+  }
 }
